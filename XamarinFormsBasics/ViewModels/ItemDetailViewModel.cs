901105909d6f6df41b06313ee5472c350d256028
John VanSickle <jwvansickle@icloud.com>
2019-06-09 13:20:16 -0400

    Initial commit, fresh scaffold project

---
+++ XamarinFormsBasics/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XamarinFormsBasics.Models;
+
+namespace XamarinFormsBasics.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
