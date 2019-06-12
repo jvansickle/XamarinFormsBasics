40e4cd511b03146e9d18103c43d08ff928c2554e
John VanSickle <jwvansickle@icloud.com>
2019-06-11 20:57:30 -0400

    Add Material Visual

--- XamarinFormsBasics.iOS/AppDelegate.cs
+++ XamarinFormsBasics.iOS/AppDelegate.cs
@@ -24,6 +24,7 @@ namespace XamarinFormsBasics.iOS
         {
             global::Xamarin.Forms.Forms.SetFlags("Shell_Experimental", "Visual_Experimental", "CollectionView_Experimental", "FastRenderers_Experimental");
             global::Xamarin.Forms.Forms.Init();
+            Xamarin.Forms.FormsMaterial.Init();
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
